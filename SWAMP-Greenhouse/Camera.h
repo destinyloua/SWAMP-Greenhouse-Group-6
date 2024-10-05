f7f833ecf4414d7b484ef68d03c3393fe9b74782
LIAM <lcabarle6383@conestogac.on.ca>
2024-10-04 22:35:20 -0400

    Updated Camera class

--- SWAMP-Greenhouse/Camera.h
+++ SWAMP-Greenhouse/Camera.h
@@ -1,2 +1,25 @@
 #pragma once
 // Camera header file
+#include <string>
+
+class Camera {
+	bool isRecording;
+	bool motionDetected;
+	int zoomLevel;
+	std::string cloudStoragePath;
+	int positionX;
+	int positionY;
+
+public:
+	Camera();
+	void viewLiveFeed();
+	void recordAndSaveToCloud();
+	void detectMotion();
+	void alertUser();
+	void zoomIn();
+	void zoomOut();
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+};
