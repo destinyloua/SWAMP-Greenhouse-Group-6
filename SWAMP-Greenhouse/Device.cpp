6207563cdcb057a19a6fa11e1bc3e9d6c5b72841
DESTINY LOUANGSOMBATH <dlouangsombath5708@conestogac.on.ca>
2024-11-07 14:52:33 -0500

    device implementation

--- SWAMP-Greenhouse/Device.cpp
+++ SWAMP-Greenhouse/Device.cpp
@@ -1,4 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Device.h"
+#include <iostream>
+
 //Device.h Implementation
 
+Device::Device(string deviceName)
+{
+	this->deviceName = deviceName; 
+}
+
+string Device::getDeviceName()
+{
+	return deviceName;
+}
--- SWAMP-Greenhouse/Device.h
+++ SWAMP-Greenhouse/Device.h
@@ -9,7 +9,8 @@ protected:
 	string deviceName; 
 public:
 	Device(string deviceName); // construct each device with a name 
-	~Device();
-	virtual void readData(string fileName); // implement logic for reading from txt file - will be different for each device 
-	virtual void control(); // implement control logic - will be different for each device 
+	string getDeviceName();
+	virtual void readData(string fileName) = 0; // implement logic for reading from txt file - will be different for each device 
+	virtual void control() = 0; // implement control logic - will be different for each device 
+	~Device() = default; 
 };
