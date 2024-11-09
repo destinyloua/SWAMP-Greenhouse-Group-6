0abd7bb7b6e31ff34acd8f357418fd238be1fc49
TylerDdao <baonamdao05@gmail.com>
2024-11-09 01:25:57 -0500

    Soil Moisture Modlue

--- SWAMP-Greenhouse/SoilMoisture.h
+++ SWAMP-Greenhouse/SoilMoisture.h
@@ -1,7 +1,19 @@
 #include "Device.h"
-
+#include <vector>
 class SoilMoisture : public Device {
+	int soilMoistureLevel;
+	int threshold;
+	vector<int> soilMoistureLog;
+	string soilMoistureData;
+	string alert;
 public:
 	SoilMoisture();
-	//void readData() override;
+	~SoilMoisture();
+	void readData();
+	void control();
+	void SetAlert(string message);
+	void SetThreshold(int value);
+	int GetThreshold();
+	bool GetIrrigationStatus();
+	void SetIrrigationStatus(bool status);
 };
