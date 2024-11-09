17737404efc1dcc3e8e69aa6c261beec4fae21d4
TylerDdao <baonamdao05@gmail.com>
2024-11-09 01:26:27 -0500

    Revert "Soil Moisture Modlue"
    This reverts commit 0abd7bb7b6e31ff34acd8f357418fd238be1fc49.

--- SWAMP-Greenhouse/SoilMoisture.h
+++ SWAMP-Greenhouse/SoilMoisture.h
@@ -1,19 +1,7 @@
 #include "Device.h"
-#include <vector>
+
 class SoilMoisture : public Device {
-	int soilMoistureLevel;
-	int threshold;
-	vector<int> soilMoistureLog;
-	string soilMoistureData;
-	string alert;
 public:
 	SoilMoisture();
-	~SoilMoisture();
-	void readData();
-	void control();
-	void SetAlert(string message);
-	void SetThreshold(int value);
-	int GetThreshold();
-	bool GetIrrigationStatus();
-	void SetIrrigationStatus(bool status);
+	//void readData() override;
 };
